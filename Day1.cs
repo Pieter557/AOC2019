56feaeef2378d68b65e32068590ad9196764682e
Pieter Demaegdt <pieter.demaegdt.ext@ts.fujitsu.com>
2019-12-01 20:29:40 +0100

    number uno

---
+++ Day1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2019
+{
+    internal class Day1
+    {
+        internal static void Part1()
+        {
+            FileInfo inputfile = new FileInfo(Program.dir + "Day1.txt");
+            var stream = inputfile.OpenText();
+            int fuel = 0;
+            while (!stream.EndOfStream)
+            {
+                var input = int.Parse(stream.ReadLine());
+                fuel += FindFuel(input);
+            }
+            Console.WriteLine(fuel);
+        }
+
+        internal static void Part2()
+        {
+            FileInfo inputfile = new FileInfo(Program.dir + "Day1.txt");
+            var stream = inputfile.OpenText();
+            int fuel = 0;
+            while (!stream.EndOfStream)
+            {
+                var input = int.Parse(stream.ReadLine());
+                fuel += FindFuelPart2(input);
+            }
+            Console.WriteLine(fuel);
+        }
+
+        internal static int FindFuel(int mass)
+        {
+            return Convert.ToInt32(Math.Floor((double)mass / 3)) - 2;
+        }
+        internal static int FindFuelPart2(int mass)
+        {
+            int addedFuel = Convert.ToInt32(Math.Floor((double)mass / 3)) - 2; ;
+
+            int fuel = addedFuel;
+            while(FindFuel(fuel) > 0)
+            {
+                addedFuel += FindFuel(fuel);
+                fuel = FindFuel(fuel);
+            }
+
+
+            return addedFuel;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AOC2019
+{
+    class Program
+    {
+        public static string dir = @"C:\Users\piete\source\repos\AOC2019\input\";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World!");
+            Day1.Part1();
+            Day1.Part2();
+            Console.ReadLine();
+        }
+    }
+}
